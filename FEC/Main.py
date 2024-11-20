6b5ec1c8db86924ddbeb33cae0b110f7577b75c9
Hanna <hania.kuczynska@gmail.com>
2024-11-20 22:59:30 +0100

    powielanie bitów
    dodano korekcje poprzez powielanie bitów

--- FEC/Main.py
+++ FEC/Main.py
@@ -1,23 +1,36 @@
 import kanaly
-import Generator
+import generator
+import korekcja_powielania_bitow
 
 def main():
-    ilosc_bitow = 6                                              # ustalanie ilości bitów w danych wejsciowych
+    ilosc_bitow = 10                                              # ustalanie ilości bitów w danych wejsciowych
 
-    dane_wejsciowe = Generator.generuj_dane(ilosc_bitow)         # Generowanie losowych bitów uzytych potem do obu modeli
+    stopien_powielenia_bitow = 15
+
+    dane_wejsciowe = generator.generuj_dane(ilosc_bitow)         # Generowanie losowych bitów uzytych potem do obu modeli
+
+    dane_wejscioewe_powielone = korekcja_powielania_bitow.powielanie(dane_wejsciowe, stopien_powielenia_bitow)
 
     #BSC
-    prawdopodobienstwo = 0.5                                     # Prawdopodobieństwo z jakim bit zostanie zmieniony na przeciwny
-    wynik_bsc = kanaly.BSC(dane_wejsciowe, prawdopodobienstwo)  # Symulacja kanału BSC z prawdopodobieństwem błędu 0.1
+    prawdopodobienstwo_BSC = 0.5                                     # Prawdopodobieństwo z jakim bit zostanie zmieniony na przeciwny
+    wynik_bsc = kanaly.BSC(dane_wejsciowe, prawdopodobienstwo_BSC)   # Symulacja kanału BSC
+    wynik_bsc_powielanie = kanaly.BSC(dane_wejscioewe_powielone, prawdopodobienstwo_BSC)  # Symulacja kanału BSC z zastosowaniem korekcji poprzez powielanie
 
     #Gilberta Elliotta
     p_dobry_do_zlego = 0.5
     p_zly_do_dobrego = 0.5
-    wynik_g_e = kanaly.gilbert_elliott(p_dobry_do_zlego, p_zly_do_dobrego, dane_wejsciowe)
+    p_bledu_dobryStan = 0.5
+    p_bledu_zlyStan = 0.5
+    wynik_g_e = kanaly.gilbert_elliott(dane_wejsciowe, p_dobry_do_zlego, p_zly_do_dobrego, p_bledu_dobryStan, p_bledu_zlyStan) # Symulacja kanału G-E
+    wynik_g_e_powielanie = kanaly.gilbert_elliott(dane_wejscioewe_powielone, p_dobry_do_zlego, p_zly_do_dobrego, p_bledu_dobryStan, p_bledu_zlyStan) # Symulacja kanału G-E z zastosowaniem korekcji poprzez powielanie
 
     print("Dane wejściowe:", dane_wejsciowe)                     # Wyświetlenie wyników
     print("Dane po przejściu przez kanał BSC:", wynik_bsc)
     print("Dane po przejściu przez kanał Gilberta Elliotta:", wynik_g_e)
+    print("Dane po przejściu przez kanał BSC z zastosowanym powieleniem:", wynik_bsc_powielanie)
+    print("Dane po przejściu przez kanał Gilberta Elliotta z zastosowanym powieleniem:", wynik_g_e_powielanie)
+    print("Skorygowane dane po BSC: ", korekcja_powielania_bitow.korektor(wynik_bsc_powielanie, stopien_powielenia_bitow))
+    print("Skorygowane dane po G-E: ", korekcja_powielania_bitow.korektor(wynik_g_e_powielanie, stopien_powielenia_bitow))
 
 if __name__ == "__main__":
     main()
--- FEC/kanaly.py
+++ FEC/kanaly.py
@@ -5,24 +5,28 @@ def BSC(dane_wejsciowe, prawdopodobienstwo):
 
     kanal = komm.BinarySymmetricChannel(prawdopodobienstwo)  # Tworzenie kanału
 
-    dane_wejsciowe=np.array(dane_wejsciowe)                  # Konwertuje na tablice NumPy
+    dane_wejsciowe = np.array(dane_wejsciowe)                  # Konwertuje na tablice NumPy
 
     return kanal(dane_wejsciowe)                             # Zwraca dane wejściowe po przejściu przez kanał BSC
 
-def gilbert_elliott(p_dobry_do_zlego, p_zly_do_dobrego, input_data):
+def gilbert_elliott(dane_wejsciowe, p_dobry_do_zlego, p_zly_do_dobrego, p_bledu_dobryStan, p_bledu_zlyStan):
     state = "dobry"                                          # Inicjalizacja stanu
     dane_wyjsciowe = []                                      # Inicjalizacja tablicy przechowującej dane wyjsciowe
 
-    for bit in input_data:
+    for bit in dane_wejsciowe:
         if state == "dobry":                                 # Stan dobry
-            bit_po_przejsciu = bit
+            if np.random.rand() < p_bledu_dobryStan:
+                dane_wyjsciowe.append(0)                 # Dodanie bitu do tablicy danych wyjsciowych z dodaniem błędu
+            else:
+                dane_wyjsciowe.append(bit)                 # Dodanie bitu do tablicy danych wyjsciowych
             if np.random.rand() < p_dobry_do_zlego:          # Losowanie czy przejść do stanu złego
-                state = 0
+                state = "zly"
         else:                                                # Stan zły
-            bit_po_przejsciu = 0
+            if np.random.rand() < p_bledu_zlyStan:
+                dane_wyjsciowe.append(0)                 # Dodanie bitu do tablicy danych wyjsciowych z dodaniem błędu
+            else:
+                dane_wyjsciowe.append(bit)                 # Dodanie bitu do tablicy danych wyjsciowych
             if np.random.rand() < p_zly_do_dobrego:          # Losujemy, czy przejść do stanu dobrego
-                state = 1
-
-        dane_wyjsciowe.append(bit_po_przejsciu)              # Dodanie bitu do tablicy danych wyjsciowych
+                state = "dobry"
 
     return dane_wyjsciowe                                    # Zwracanie tablicy danych wyjsciowych
