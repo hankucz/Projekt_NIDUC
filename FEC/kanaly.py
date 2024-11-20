bd13995ec8123a9173c6e3844460bb8183dc53a0
AmeliaSroczynska <271223@student.pwr.edu.pl>
2024-11-20 03:16:00 +0100

    Zaimplementowałam kanal gilberta elliotta

--- FEC/kanaly.py
+++ FEC/kanaly.py
@@ -10,3 +10,20 @@ def BSC(dane_wejsciowe, prawdopodobienstwo):
 
     return kanal(dane_wejsciowe)                             # Zwraca dane wejściowe po przejściu przez kanał BSC
 
+def gilbert_elliott_model(p_dobry_do_zlego, p_zly_do_dobrego, input_data):
+    state = "dobry"                                          # Inicjalizacja stanu
+    dane_wyjsciowe = []                                      # Inicjalizacja tablicy przechowującej dane wyjsciowe
+
+    for bit in input_data:
+        if state == "dobry":                                 # Stan dobry
+            bit_po_przejsciu = bit
+            if np.random.rand() < p_dobry_do_zlego:          # Losowanie czy przejść do stanu złego
+                state = 0
+        else:                                                # Stan zły
+            bit_po_przejsciu = 0
+            if np.random.rand() < p_zly_do_dobrego:          # Losujemy, czy przejść do stanu dobrego
+                state = 1
+
+        dane_wyjsciowe.append(bit_po_przejsciu)              # Dodanie bitu do tablicy danych wyjsciowych
+
+    return dane_wyjsciowe                                    # Zwracanie tablicy danych wyjsciowych
